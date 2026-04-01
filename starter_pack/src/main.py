6a2087e4f0c806b74c34f0a732097b747388ba5b
muradtt011 <muradtt011@gmail.com>
2026-04-01 22:34:49 +0400

    Updating Main and NN model

--- starter_pack/src/main.py
+++ starter_pack/src/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import itertools
+import argparse
 matplotlib.use("TkAgg")
 
 from validation import Validation
@@ -43,89 +44,165 @@ y_m_test=data_moons['y_test']
 
 
 data = [(X_d_train,y_d_train,X_d_val,y_d_val),(X_l_train,y_l_train,X_l_val,y_l_val),(X_m_train,y_m_train,X_m_val,y_m_val)]
+test_data = [(X_d_test,y_d_test),(X_l_test,y_l_test),(X_m_test,y_m_test)]
 data_name = ["Digits","Linear gaussian","Moons"]
 
-for i,d in enumerate(data):
-    print(f"Data set {data_name[i]}")        
-    validation_s = Validation(SoftMaxClassification,*d)
-    acc,model_s = validation_s.fit()
-    validation_s.report()
 
-
-
-for d in data[1:]:
-    fig, axs = plt.subplots(1, 2,figsize=(10,4),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
-    for i,(lr,e) in enumerate(itertools.product([0.05],[200])):
-        ss = SoftMaxClassification(learning_rate=lr,max_iter=e)
-        ss.fit(*d[0:2])
-        ss.plot_loss(ax=axs[0])
-        ss.plot_decison_boundary(*d[0:2],ax=axs[1])
-
-
-
-for i,d in enumerate(data):
-    print(f"Data set {data_name[i]}")
-    validation_nn = Validation(NeuralNetwork,X_d_train,y_d_train,X_d_val,y_d_val,params={"size":[32],"optimizer":"momentum","epochs":200,"learning_rate":0.05})
-    acc_m,model_nn = validation_nn.fit()
-    validation_nn.report()
+def gradient_sanity_check():
+    SoftMaxClassification.gradient_check(X_d_train[:30],y_d_train[:30])
+    NeuralNetwork.gradient_check(X_d_train[:30],y_d_train[:30])
 
 
 
 
-for j,d in enumerate(data):
-    print(f"Data set {data_name[j]}")
-    fig, axs = plt.subplots(2, 3,figsize=(18, 12),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
-    for i,(o,s,lr) in enumerate(itertools.product(["sgd","adam","momentum"],[32],[0.001,0.05])):
-        nn = NeuralNetwork(size=[s],optimizer=o,learning_rate=lr)
-        nn.fit(*d[:2])
-        ax = axs[i//3,i%3]
-        nn.plot_loss(ax)
+def one_failure_case_analysis():
+    fig,axs = plt.subplots(2,2,figsize=(18,12))
+    ss = SoftMaxClassification()
+    Validation.test_model(ss,X_d_train[:30],y_d_train[:30],X_d_val,y_d_val,X_d_test,y_d_test,axes = axs[0])
+
+    nn = NeuralNetwork(optimizer="adam",learning_rate=0.001,size=[64])
+    Validation.test_model(nn,X_d_train[:30],y_d_train[:30],X_d_val,y_d_val,X_d_test,y_d_test,axes = axs[1])
+    plt.show()
+    
+    fig, axs = plt.subplots(2, 2,figsize=(18, 12),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
+    idxs = make_bias(y_d_train,1)
+    plot_hist(y_d_train,idxs,axs[0])
+    nn = NeuralNetwork(optimizer="adam",learning_rate=0.3,size=[8])
+    Validation.test_model(nn,X_d_train[idxs],y_d_train[idxs],X_d_val,y_d_val,X_d_test,y_d_test,axes=axs[1])
+    plt.show()
 
 
 
-for j,d in enumerate(data):
-    print(f"Data set {data_name[j]}")
-    fig, axs = plt.subplots(2, 3,figsize=(18, 12),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
-    for i,(o,s,lr) in enumerate(itertools.product(["sgd","adam","momentum"],[32],[0.001,0.05])):
-        nn = NeuralNetwork(size=[s],optimizer=o,learning_rate=lr)
-        nn.fit(*d[:2])
-        ax = axs[i//3,i%3]
-        nn.plot_decision_boundary(*d[:2],ax)
-
-
-
-
-idxs = make_bias(y_d_train,1)
-plot_hist(y_d_train,idxs)
-
-
-nn = NeuralNetwork(optimizer="adam",learning_rate=0.2,size=[8])
-test_model(nn,X_d_train[idxs],y_d_train[idxs],X_d_test,y_d_test,epochs=200)
-
-
-idxs = make_bias(y_l_train,1)
-plot_hist(y_l_train,idxs)
-
-ss = SoftMaxClassification(learning_rate=0.05,batch_size=32)
-test_model(ss,X_l_train[idxs],y_l_train[idxs],X_l_val,y_l_val)
-
-
-nn = NeuralNetwork(optimizer="adam",learning_rate=0.001,size=[64])
-test_model(nn,X_d_train[:30],y_d_train[:30],X_d_val,y_d_val,epochs=200)
 
 
 
 def compare_at_fixed_pca_dimension(dimensions:list,estimator,X_Train,y_train,X_val,y_val):
     X_mean,X_centered,Vt = pca_dimensions(X_Train)
     X_d_val_centered = X_val - X_mean
-    for d in dimensions:
-        print(f"PCA dimension {d}")
+    fig, axs = plt.subplots(1, 3,figsize=(18, 6),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
+    for i,d in enumerate(dimensions):
         X_d_t = X_centered @ Vt[:d].T 
         X_d_v = X_d_val_centered @ Vt[:d].T
-        validation_pca_s = Validation(estimator,X_d_t,y_train,X_d_v,y_val)
-        acc,model_pca = validation_pca_s.fit()
-        model_pca.plot_loss()
-        validation_pca_s.report()
+        model_pca = estimator()
+        model_pca.fit(X_d_t,y_train)
+        acc = accuracy(model_pca.predict(X_d_v),y_val)
+        text = f"Data set {data_name[0]}\nDimension: {d}\nAccuracy :{acc:.4f}"
+        axs[i].text(0.5,0.5,text,color="black",fontsize=14,ha="center",transform=axs[i].transAxes)
+        model_pca.plot_loss(axs[i])
+    plt.show()
 
-plot_scree(X_d_train)
-compare_at_fixed_pca_dimension([10,20,40],SoftMaxClassification,X_d_train,y_d_train,X_d_val,y_d_val)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cpad", action="store_true", help="Checkpoint policy across datasets")
+    parser.add_argument("--ra", action="store_true", help="Required ablations")
+    parser.add_argument("--pca", action="store_true", help="Track A: PCA/SVD and input geometry")
+    parser.add_argument("--rss", action="store_true", help="Repeated-seed statistics")
+    parser.add_argument("--isc", action="store_true", help="Implementation sanity checks")
+    parser.add_argument("--rce", action="store_true", help="Required core experiments")
+   
+
+    args = parser.parse_args()
+    if args.ra:
+        one_failure_case_analysis()
+        fig,axs =  plt.subplots(1,3,figsize=(18,6),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
+        
+        # Optimizer study on digits.
+        for i,o in enumerate(["adam","momentum","sgd"]):
+            text = f"Data set {data_name[0]};Optimizer: {o.capitalize()}"
+            model_nn = NeuralNetwork(optimizer=o)
+            model_nn.fit(X_d_train,y_d_train)
+            axs[i].text(0.5,0.5,text,color="black",fontsize=14,ha="center",transform=axs[i].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+            axs[i].text(0.5,0.5,text,color="black",fontsize=14,ha="center",transform=axs[i].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+            model_nn.plot_loss(axs[i])
+        plt.show()
+        
+        #Capacity ablation on moons. Use hidden widths {2, 8, 32}. Interpret what changes in the learned decision boundary.
+        fig, axs = plt.subplots(3, 3,figsize=(21, 18),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
+        for i,(o,s) in enumerate(itertools.product(["sgd","adam","momentum"],[2,8,32])):
+            nn = NeuralNetwork(size=[s],optimizer=o)
+            nn.fit(X_m_train,y_m_train)
+            ax = axs[i//3,i%3]
+            nn.plot_decision_boundary(X_m_train,y_m_train,ax)
+        plt.show()
+    
+    if args.isc:
+        gradient_sanity_check()
+
+        # successful overfitting of a very small subset of training examples,
+        # evidence that the loss decreases on a tiny subset after a few updates,
+        fig,axs = plt.subplots(2,2,figsize=(18,12))
+        ss = SoftMaxClassification()
+        Validation.test_model(ss,X_d_train[:30],y_d_train[:30],X_d_val,y_d_val,X_d_test,y_d_test,epochs=200,axes = axs[0])
+        nn = NeuralNetwork(optimizer="adam",learning_rate=0.001,size=[64])
+        Validation.test_model(nn,X_d_train[:30],y_d_train[:30],X_d_val,y_d_val,X_d_test,y_d_test,epochs=200,axes = axs[1])
+        plt.show()
+        
+        #confirmation that predicted class probabilities sum to one,
+        s = X_d_test.shape[0]
+        ss = SoftMaxClassification(learning_rate=0.05,batch_size=32)
+        ss.fit(X_d_train,y_d_train)
+        print(ss._predict(X_d_test[np.random.randint(0,s)]).sum(axis=1),nn._predict(X_d_test[np.random.randint(0,s)]).sum(axis=1))
+    
+    if args.pca:
+        # one scree plot
+        plot_scree(X_d_train)
+        #  one small softmax comparison at fixed PCA dimensions m ∈ {10, 20, 40},
+        compare_at_fixed_pca_dimension([10,20,40],SoftMaxClassification,X_d_train,y_d_train,X_d_val,y_d_val)
+
+        # one 2D PCA visualization of the digits data,
+        X_mean,X_centered,Vt = pca_dimensions(X_d_train)
+        X_2d = X_centered @ Vt[:2].T
+        plt.scatter(X_2d[:,0], X_2d[:,1], c=y_d_train, cmap='tab10')
+        plt.title("2D PCA visualization of the digits data")
+        plt.show()
+
+    if args.rss or args.cpad:
+        fig,axs =  plt.subplots(1,2,figsize=(18,6))
+        print(f"--- Statistics for 5 Seeds ---")
+        text = f"Data set {data_name[0]}\nOptimizer: SGD"
+        validation_s = Validation(SoftMaxClassification,*data[0],*test_data[0])
+        acc,model_s = validation_s.fit()
+        validation_s.report(axs[0])
+        model_s.plot_loss(axs[1])
+        axs[0].text(0.5,0.5,text,color="black",fontsize=14,ha="center",transform=axs[0].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+        axs[1].text(0.5,0.5,text,color="black",fontsize=14,ha="center",transform=axs[1].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+        plt.show()
+    if args.rce:
+        # Train and compare both models on the linear Gaussian task. Include decision-boundary plots.
+        # Train and compare both models on the moons task. Include decision-boundary plots.
+        for i,d in enumerate(data[1:],1):
+            text = f"Data set:{data_name[i]}"
+            fig, axs = plt.subplots(2, 2,figsize=(16,10),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
+            for ax in axs.flat:
+                ax.text(0.5,0.5,text,color="black",fontsize=14,ha="center",transform=ax.transAxes,bbox=dict(facecolor='white', alpha=0.7))
+                
+            ss = SoftMaxClassification()
+            ss.fit(*d[0:2])
+            axs[0,0].text(0.5,0.4,f"Accuracy :{accuracy(ss.predict(test_data[i][0]),test_data[i][1]):.4f}",color="black",fontsize=14,ha="center",transform=axs[0,0].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+            ss.plot_loss(ax=axs[0,0])
+            ss.plot_decision_boundary(*d[0:2],ax=axs[0,1])
+            
+            nn = NeuralNetwork()
+            nn.fit(*d[0:2])
+            axs[1,0].text(0.5,0.4,f"Accuracy :{accuracy(nn.predict(test_data[i][0]),test_data[i][1]):.4f}",color="black",fontsize=14,ha="center",transform=axs[1,0].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+            nn.plot_loss(ax=axs[1,0])
+            nn.plot_decision_boundary(*d[0:2],ax=axs[1,1])
+            plt.show()
+
+        # Train and compare both models on the fixed digits benchmark using the same preprocessingand split.
+        text = f"Data set:{data_name[0]}"
+        fig, axs = plt.subplots(1, 2,figsize=(16,6),gridspec_kw={"hspace": 0.4, "wspace": 0.3})
+
+        ss = SoftMaxClassification()
+        ss.fit(X_d_train,y_d_train)
+        axs[0].text(0.5,0.5,f"Data set:{data_name[0]}\nAccuracy {accuracy(ss.predict(X_d_test),y_d_test):.4f}",color="black",fontsize=14,ha="center",transform=axs[0].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+        ss.plot_loss(ax=axs[0])
+
+
+        nn = NeuralNetwork()
+        nn.fit(X_d_train,y_d_train)
+        axs[1].text(0.5,0.5,f"Data set:{data_name[0]}\nAccuracy {accuracy(nn.predict(X_d_test),y_d_test):.4f}",color="black",fontsize=14,ha="center",transform=axs[1].transAxes,bbox=dict(facecolor='white', alpha=0.7))
+        nn.plot_loss(ax=axs[1])
+        plt.show()
--- starter_pack/src/nn.py
+++ starter_pack/src/nn.py
@@ -3,31 +3,35 @@ import copy
 import matplotlib.pyplot as plt
 
 
-from utils import softmax
+from utils import *
+
 
 
 
 class NeuralNetwork():
-    def __init__(self,size=[32],lamda = 1e-4,batch_size=64,learning_rate = 0.05, optimizer="sgd",epochs = 200,activation_functions = (np.tanh,softmax)):
+    def __init__(self,size=[32],lamda = 1e-4,batch_size=64,learning_rate = None, optimizer="sgd",epochs = 200,activation_functions = (np.tanh,softmax),init_weights=True):
         self.size = size
         self.batch_size = batch_size
-        self.learning_rate = learning_rate
+        if not learning_rate:
+            self.learning_rate = 0.001 if optimizer=="adam" else 0.05
+        else:
+            self.learning_rate = learning_rate
         self.cache = {}
         self.optimizer=optimizer
         self.lamda = lamda
         self.epochs = epochs
         self.loss=[]
         self.activation_functions = activation_functions
-       
-        
+        self.init_weights = init_weights
     
     def fit(self,X_train,y_train):
-        self._initialize_weights(X_train,y_train)
+        self.n,self.m = X_train.shape
+        self.labels = np.unique(y_train)
         
-        Y = np.zeros((self.n,len(self.n_class)))
-        for i, val in enumerate(y_train):
-            Y[i,np.where(self.n_class == val)] = 1
-            
+        if self.init_weights:
+            self._initialize_weights(X_train,y_train) 
+        Y = one_hot_encoder(y_train,self.labels)
+        
         idxs = np.arange(self.n)
         for e in range(self.epochs):
             np.random.shuffle(idxs)
@@ -46,19 +50,18 @@ class NeuralNetwork():
             y_pred = self._predict(X_train)
             L = self._calculate_loss(Y,y_pred)
             self.loss.append(L)
+
             
     def _initialize_weights(self,X_train,y_train):
-        self.n,self.m = X_train.shape
-        self.n_class = np.unique(y_train)
         
-        size = [self.m] + self.size + [len(self.n_class)]
+        size = [self.m] + self.size + [len(self.labels)]
         
-        self.w = [np.random.randn(size[i+1],size[i])*np.sqrt(1/size[i]) for i in range(len(size)-1)]
+        self.W = [np.random.randn(size[i+1],size[i])*np.sqrt(1/size[i]) for i in range(len(size)-1)]
         self.b = [np.zeros((1,size[i+1])) for i in range(len(size)-1)]
     
     def _forward(self,X_batch):
         A = X_batch
-        for i,(w,b) in enumerate(zip(self.w,self.b)):
+        for i,(w,b) in enumerate(zip(self.W,self.b)):
             activation = self.activation_functions[i]
             A = activation(A @ w.T + b)
             self.cache[f"A{i}"] = A
@@ -77,12 +80,12 @@ class NeuralNetwork():
     def back_propagation(self,X,Y):
         dLdZ= (self.cache["A1"] - Y)/Y.shape[0]
         dZdW2 = self.cache["A0"]
-        self.cache["dW2"] = dLdZ.T @ dZdW2 + self.lamda*self.w[1]
+        self.cache["dW2"] = dLdZ.T @ dZdW2 + self.lamda*self.W[1]
         self.cache["db2"] = np.sum(dLdZ, axis=0, keepdims=True)
         
-        dLdH =  dLdZ @ self.w[1]
+        dLdH =  dLdZ @ self.W[1]
         dLdK = dLdH * (1 - self.cache["A0"]**2)
-        self.cache["dW1"] = dLdK.T @ X + self.lamda * self.w[0]
+        self.cache["dW1"] = dLdK.T @ X + self.lamda * self.W[0]
         self.cache["db1"] = np.sum(dLdK, axis=0, keepdims=True)
         
         # print("softmax",self.cache["softmax"].shape)
@@ -92,11 +95,11 @@ class NeuralNetwork():
         # print("dLdK",dLdK.shape)
 
     def _l2(self):
-        return 0.5 * self.lamda * np.sum([np.sum(np.power(w,2)) for w in self.w]) 
+        return 0.5 * self.lamda * np.sum([np.sum(np.power(w,2)) for w in self.W]) 
     
     def sgd(self):
-        for i in range(len(self.w)):
-            self.w[i]-= self.learning_rate * self.cache[f"dW{i+1}"]
+        for i in range(len(self.W)):
+            self.W[i]-= self.learning_rate * self.cache[f"dW{i+1}"]
             self.b[i]-= self.learning_rate * self.cache[f"db{i+1}"]
 
         
@@ -104,32 +107,32 @@ class NeuralNetwork():
 
     def momentum(self,beta = 0.9):
         if not hasattr(self,"v_w"):
-            self.v_w = [np.zeros_like(w) for w in self.w]
+            self.v_w = [np.zeros_like(w) for w in self.W]
             self.v_b = [np.zeros_like(b) for b in self.b]
             
-        for i in range(len(self.w)):
+        for i in range(len(self.W)):
             self.v_w[i] = beta*self.v_w[i] + (1-beta)*self.cache[f"dW{i+1}"]
             self.v_b[i] = beta*self.v_b[i] + (1-beta)*self.cache[f"db{i+1}"]
 
-            self.w[i] -= self.learning_rate * self.v_w[i]
+            self.W[i] -= self.learning_rate * self.v_w[i]
             self.b[i] -= self.learning_rate * self.v_b[i]
         
 
     def adam(self,beta_1=0.9,beta_2=0.999, epsilon=1e-8):
         if not hasattr(self, "m_w"):
-            self.m_w = [np.zeros_like(w) for w in self.w]
-            self.v_w = [np.zeros_like(w) for w in self.w]
+            self.m_w = [np.zeros_like(w) for w in self.W]
+            self.v_w = [np.zeros_like(w) for w in self.W]
             self.m_b = [np.zeros_like(b) for b in self.b]
             self.v_b = [np.zeros_like(b) for b in self.b]
             self.t = 0
         self.t += 1
 
-        for i in range(len(self.w)):
+        for i in range(len(self.W)):
             self.m_w[i] = beta_1 * self.m_w[i] + (1 - beta_1) * self.cache[f"dW{i+1}"]
             self.v_w[i] = beta_2 * self.v_w[i] + (1 - beta_2) * (self.cache[f"dW{i+1}"]**2)
             m_hat = self.m_w[i] / (1 - beta_1**self.t)
             v_hat = self.v_w[i] / (1 - beta_2**self.t)
-            self.w[i] -= self.learning_rate * m_hat / (np.sqrt(v_hat) + epsilon)
+            self.W[i] -= self.learning_rate * m_hat / (np.sqrt(v_hat) + epsilon)
             
             self.m_b[i] = beta_1 * self.m_b[i] + (1 - beta_1) * self.cache[f"db{i+1}"]
             self.v_b[i] = beta_2 * self.v_b[i] + (1 - beta_2) * (self.cache[f"db{i+1}"]**2)
@@ -139,7 +142,7 @@ class NeuralNetwork():
 
     def _predict(self, X):
         A = X
-        for i,(w,b) in enumerate(zip(self.w,self.b)):
+        for i,(w,b) in enumerate(zip(self.W,self.b)):
             A = self.activation_functions[i](A @ w.T + b)
         return A
 
@@ -148,30 +151,28 @@ class NeuralNetwork():
         model = cls(**copy.deepcopy(params))
         model.fit(X,y)
         
-        Y = np.zeros((model.n,len(model.n_class)))
-        for i, val in enumerate(y):
-            Y[i, val] = 1
+        Y = one_hot_encoder(y,model.labels)
         model._forward(X)
         model.back_propagation(X, Y)    
-        max_diff = 0;
-        for l in range(len(model.w)):
+        max_diff = 0
+        for l in range(len(model.W)):
             grad_analytic = np.copy(model.cache[f"dW{l+1}"])
             grad_num = np.zeros_like(grad_analytic)
             for i in range(grad_analytic.shape[0]):
                 for j in range(grad_analytic.shape[1]):
-                    model.w[l][i,j] += epsilon
+                    model.W[l][i,j] += epsilon
                     y_plus = model._predict(X)
                     L1 = model._calculate_loss(Y,y_plus)
     
-                    model.w[l][i,j] -= 2*epsilon
+                    model.W[l][i,j] -= 2*epsilon
                     y_minus = model._predict(X)
                     L2 = model._calculate_loss(Y,y_minus)
-                    model.w[l][i,j] += epsilon
+                    model.W[l][i,j] += epsilon
                     grad_num[i,j] = (L1 - L2)/(2*epsilon)
                 
             diff = np.linalg.norm(grad_analytic - grad_num) / (np.linalg.norm(grad_analytic) + np.linalg.norm(grad_num))
-            print("Gradient check difference:", diff)
             max_diff = max(max_diff,diff)
+        print(f"Gradient check difference for {cls.__name__}:", max_diff)
         return max_diff
     
     def predict(self, X):
@@ -205,17 +206,18 @@ class NeuralNetwork():
         ax.contourf(xx, yy, Z, alpha=0.3)
         ax.contour(xx, yy, Z, colors="red", linewidths=1)
         ax.scatter(X_2d[:, 0], X_2d[:, 1], c=y, edgecolors="k", linewidths=0.5)
-        ax.set_title(f"{self.__class__.__name__} Decision Boundary\n {'(PCA 2D)' if X.shape[1] > 2 else ''} \nOptimizer:{self.optimizer.capitalize()}; Hidden Layer Size:{self.size};\nLearning rate:{self.learning_rate};Epochs:{self.epochs};Loss: {self.loss[-1]:.4f}")
+        ax.set_title(f"{self.__class__.__name__}\nDecision Boundary {'(PCA 2D)' if X.shape[1] > 2 else ''} \nOptimizer:{self.optimizer.capitalize()}; Hidden Layer Size:{self.size};\nLearning rate:{self.learning_rate};Epochs:{self.epochs};Loss: {self.loss[-1]:.4f}")
         ax.set_xlabel("PC1" if X.shape[1] > 2 else "X0")
         ax.set_ylabel("PC2" if X.shape[1] > 2 else "X1")
                 
     def plot_loss(self,ax=None):
         if ax is None:
             fig, ax = plt.subplots(1,1,figsize=(8,6))
-        ax.set_title(f"Optimizer:{self.optimizer.capitalize()}; Hidden Layer Size:{self.size};\nLearning rate:{self.learning_rate}")
+        ax.set_title(f"{self.__class__.__name__}\nOptimizer:{self.optimizer.capitalize()}; Hidden Layer Size:{self.size};\nLearning rate:{self.learning_rate}")
         ax.plot(range(len(self.loss)),self.loss)
         ax.set_ylabel(f"Training Log Loss")
         ax.set_xlabel("Epochs")
         ax.set_yticks(np.linspace(min(self.loss), max(self.loss), 10))
         ax.grid()
+        
         
